f0edfa489735ebcaa9bebc04b62a8fd678b2d318
Priyanshu-Xeno <priyanshu.singh@xeno.in>
2023-12-28 16:09:00 +0530

    Initial commit

---
+++ sdkapp/src/main/java/com/example/sdkapp/activitysdk.kt
@@ -0,0 +1,11 @@
+package com.example.sdkapp
+
+import android.content.Context
+import android.widget.Toast
+
+class activitysdk {
+    fun solve(context: Context){
+        Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show()
+
+    }
+}
